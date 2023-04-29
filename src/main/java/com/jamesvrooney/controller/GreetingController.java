add0259f470f04fd9b28d0a8adb1d31e3939ceaa
jamesvrooney <jamesvrooney@hotmail.com>
2023-04-29 11:39:00 +0100

    Added new argument7.

--- src/main/java/com/jamesvrooney/controller/GreetingController.java
+++ src/main/java/com/jamesvrooney/controller/GreetingController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-  public void sayHello(String newCustomer6){
+  public void sayHello(String newCustomer7){
   }
 }
